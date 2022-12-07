da6d98aebe8ea88911dc987c01c63459130dbcc9
Ousmane Konate <konatealim@gmail.com>
2022-12-07 23:33:18 +0100

    add

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
