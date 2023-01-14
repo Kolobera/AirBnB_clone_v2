b4d2884f8a3919be7f88d5127cb0d4a6e07c8566
Ousmane Konate <konatealim@gmail.com>
2023-01-14 22:17:12 +0100

    add

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Displays hbnbfilters"""
 from flask import Flask, render_template
-from models import storage, State, Amenity
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def filters_hbnb():
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
     return render_template('10-hbnb_filters.html',
                            states=states, amenities=amenities)
 
